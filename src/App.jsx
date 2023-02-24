e796b540832818d15a74e2251cd8e743720a72f0
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-02-23 21:46:09 -0300

    created all color flipper

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import './App.css'
+import { useState } from 'react'
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 255)
+}
 
 function App() {
-    return null
+  const [color, setColor] = useState('rgb(255, 255, 255)')
+
+  return (
+    <div className="container parent" style={{ backgroundColor: color }}>
+      <div className="child">
+        <div className="color-info">
+          Background color:
+          <div className="color-value" style={{ color: color }}>
+            {color}
+          </div>
+        </div>
+        <div className="container">
+          <button
+            className="button"
+            onClick={() => {
+              setColor(
+                `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`
+              )
+            }}
+          >
+            Click me
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
